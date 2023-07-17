286ee71b2b1807a726c00d45d140d14cd81667a1
Olivverde <dele19270@uvg.edu.gt>
2023-07-17 16:20:08 -0600

    Test

--- Main.py
+++ Main.py
@@ -1,35 +1,62 @@
 from antlr4 import *
+from antlr4 import ParseTreeVisitor
+from antlr4.tree.Tree import TerminalNodeImpl
 from testLexer import testLexer
 from testParser import testParser
 from antlr4.tree.Trees import Trees
 from ast_v import *
 import pydot
 
-def main():
-    file_route = "test.yapl"
+class Lex_Ser():
     
-    with open(file_route, 'r') as file:
-        input_stream = InputStream(file.read())
+    def main(self):
         
-        lexer = testLexer(input_stream)
-        token_stream = CommonTokenStream(lexer)
-        parser = testParser(token_stream)
-        
-        tree = parser.prog()
-        print(Trees.toStringTree(tree, None, parser))
-        
-        visitor = TransformDot()
-        visitor.visit(tree)
-        dot = visitor.getDot()
-        with open('ast.dot', 'w') as file:
-            file.write(dot)
-        
-if __name__ == "__main__":
-    main()
+        with open("test.yapl", 'r') as file:
+            input_stream = InputStream(file.read())
+            
+            lexer = testLexer(input_stream)
+            token_stream = CommonTokenStream(lexer)
+            parser = testParser(token_stream)
+            
+            tree = parser.prog()        
+            visitor = TransformDot()
+            visitor.visit(tree)
+            dot = visitor.getDot()
+            with open('ast.dot', 'w') as file:
+                file.write(dot)
+            
+class TransformDot(ParseTreeVisitor):
+    def __init__(self):
+        super().__init__()
+        self.dot = 'digraph AST {\n'
+        self.id = 0
+
+    def visitChildren(self, node):
+        node_id = self.id
+        for i in range(node.getChildCount()):
+            child = node.getChild(i)
+            if not isinstance(child, TerminalNodeImpl):
+                self.id += 1
+                child_id = self.id
+                self.dot += '  node{} [label="{}"];\n'.format(child_id, child.getText())
+                self.dot += '  node{} -> node{};\n'.format(node_id, child_id)
+                self.visit(child)
+
+    def terminalNode(self, node):
+        self.dot += '  node{} [label="{}"];\n'.format(self.id, node.getText())
+        if self.id != 0:
+            self.dot += '  node{} -> node{};\n'.format(self.id - 1, self.id)
+
+    def getDot(self):
+        return self.dot + '}\n'
+
+    
+LS = Lex_Ser()
+LS.main()
     
 
-    graphs = pydot.graph_from_dot_file('ast.dot')
-    graph = graphs[0]
-    graph.write_png('out.png')
+    # graphs = pydot.graph_from_dot_file('ast.dot')
+    # graph = graphs[0]
+    # graph.write_png('out.png')
     # graph.write_png('somefile.png')
     

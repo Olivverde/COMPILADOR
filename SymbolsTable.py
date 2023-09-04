abca14408380e1a5aa1694cd5f15b40dcd701fda
Levi Tamath <tam19365@uvg.edu.gt>
2023-09-03 20:47:48 -0600

    ClassTable() fixed and implementation of  ErrorTypes

--- SymbolsTable.py
+++ SymbolsTable.py
@@ -111,30 +111,123 @@ class ParameterTable():
         self.param_table = []  # Clear the parameter table
 
 
+"""
+        _Author_
+        Laura
+        
+        _summary_
+        Represents a table for managing classes and their properties within a compiler or interpreter. 
+"""
 class ClassTable():
-    def __init__(self) -> None:
-        self.pretty_table = PrettyTable()
-        self._classes = []
-    def add(self,type, id, inheritance):
-        self._classes.append({
-            'Type': type,
-            'ID': id,
-            'Inheritance': inheritance
-        })
-    def lookup(self, type):
-        for _class in self._classes:
-            if _class['Type'] == type:
-                return _class
-        return 0
-    
-    def totable(self):
-        self.pretty_table.field_names = ['Type', 'ID', 'Inheritance']
-        for i in self._classes:
-            self.pretty_table.add_row(list(i.values()))
+    def __init__(self):
+        # Initialize the class table with a PrettyTable for formatting.
+        self.to_pretty = PrettyTable()
 
-        print(Fore.CYAN + "\nClases" + Style.RESET_ALL)
-        print(self.pretty_table)
-        self.pretty_table.clear_rows()
+        # Initialize the class table with default class entries.
+        self.class_table = []
+        self.add_class('IO', 'IO', "", ["out_string", "out_int", "in_string", "in_int"])
+        self.add_class('Object', 'Object', "", ["abort", "type_name", "copy"])
+        self.add_class('String', 'String', "", ["length", "concat", "substr"])
+        self.add_class('Int', 'Int', "", [])
+        self.add_class('Bool', 'Bool', "", [])
+
+    def add_class(self, id, desc, inherits, methods):
+        # Add a new class entry to the class table.
+        # Parameters:
+        #   - id: The class name.
+        #   - desc: The class description.
+        #   - inherits: The parent class the class inherits from.
+        #   - methods: A list of methods associated with the class.
+        self.class_table.append({
+            "Id": id,
+            "Description": desc,
+            "inherits": inherits,
+            "methods": methods
+        })
+
+    def search(self, id):
+        # Search for a class by its identifier.
+        # Parameters:
+        #   - id: The class name to search for.
+        # Returns:
+        #   - The class entry if found, None otherwise.
+        this_class = self.class_table.copy()
+        this_class.reverse()
+        for class_ in this_class:
+            if class_["Id"] == id:
+                return class_
+        return None
+
+    def add(self, class_name, method_name):
+        # Add a method to a class by its name.
+        for class_ in self.class_table:
+            if class_["Id"] == class_name:
+                class_["methods"].append(method_name)
+
+    def toTable(self):
+        # Display the class table in a formatted table.
+        self.to_pretty.field_names = ["Id", "Description", "inherits", "Methods"]
+        for class_ in self.class_table:
+            self.to_pretty.add_row(list(class_.values()))
+        print(self.to_pretty)
+        self.to_pretty.clear_rows()  # Clear the table for future use
+
+
+
+"""
+        _Author_
+        Laura
+        
+        _summary_
+        Represents a collection of error messages and provides a mechanism for adding and retrieving
+        errors encountered during code analysis or compilation.
+"""
+class ErrorTypes():
+    def __init__(self):
+        # Initialize the error table.
+        self.error_table = []
+
+        # Define error messages as constants.
+        self.redef = 'It is not possible to redefine an identifier more than once within the same scope'
+        self.no_def = 'It is not possible to use an identifier that has not been defined'
+        self.no_type = 'It is not possible to use a type that has not been defined'
+        self.no_main = 'It is not possible to execute the program without a main function'
+        self.main_her = 'The Main class cannot inherit from another class'
+        self.no_her = 'It is not possible to inherit from a class that has not been defined'
+        self.self_her = 'Recursive inheritance is not allowed'
+        self.dif_no_par = 'It is not possible to call a function with a different number of parameters than defined'
+        self.dif_type_par = 'It is not possible to call a function with a parameter type different from the defined type'
+        self.dif_type_ret = 'It is not possible to return a different type than the defined return type'
+        self.no_ret = 'It is not possible to return a value in a function that does not return anything'
+        self.arithmetic_no_int = 'Arithmetic operations can only be performed with int or string concatenation types'
+        self.cond_no_bool = 'Comparison operations require operands of the same data type'
+        self.not_bool = 'The "not" operator can only be used on boolean expressions'
+        self.not_int = 'The negation operator can only be used on integer expressions'
+        self.if_no_bool = 'An "if" condition must have a boolean expression'
+        self.if_no_match = 'The types of "if" branches do not match'
+        self.while_no_bool = 'A "while" condition must have a boolean expression'
+        self.diff_asign = 'It is not possible to assign a different type than the one defined'
+        self.no_method = 'It is not possible to call a method or variable that has not been defined'
+
+    def add_error(self, error, line, col):
+        # Add a new error entry to the error table.
+        # Parameters:
+        #   - error: The error message.
+        #   - line: The line number where the error occurred.
+        #   - col: The column number where the error occurred.
+        self.error_table.append({
+            "error": error,
+            "line": line,
+            "col": col
+        })
+    
+    def get_errors(self):
+        # Get a list of error messages in a user-friendly format.
+        ui_errors = []
+        for error in self.error_table:
+            ui_errors.append(f'Error: {error["error"]} at line {error["line"]}, column {error["col"]}')
+        return ui_errors
+
 
 
 """
